52fe613503ab314e5ac60ca1931775f1873044fa
Paola Aguilar <pao.aquev29@gmail.com>
2021-01-06 01:03:29 -0600

    login and signup are working, but without any style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import Container from "./components/container/Container.js";
+import React from "react";
+import Container from "./components/container/Container";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/login/Login";
+import SignUp from "./components/login/SignUp";
+import { AuthProvider } from "./components/Auth";
+import PrivateRoute from "./components/PrivateRoute";
 import "./App.css";
 
-function App() {
-  return <Container />;
-}
+const App = () => {
+  return (
+    <Container>
+      <div className="site-layout-content">
+        <AuthProvider>
+          <Router>
+            <div>
+              <PrivateRoute exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={SignUp} />
+            </div>
+          </Router>
+        </AuthProvider>
+      </div>
+    </Container>
+  );
+};
 
 export default App;
--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,19 +1,14 @@
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
+import Header from "./../Header";
 import "./container.css";
 
-const Container = () => {
-  const { Header, Content, Footer } = Layout;
+const Container = ({ children }) => {
+  const { Content, Footer } = Layout;
 
   return (
     <Layout className="layout">
-      <Header>
-        <h1 className="title">Sistema de incapacidades</h1>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal"></Menu>
-      </Header>
-      <Content>
-        <div className="site-layout-content">Content</div>
-      </Content>
+      <Header />
+      <Content>{children}</Content>
       <Footer style={{ textAlign: "center" }}>Bootcamp - ReactJs ©2021</Footer>
     </Layout>
   );
